692d1066decaf4217b4aafcf2e5b7164f2c6ca1d
Alfred Takala <alfredtakala@yandex.com>
2021-12-16 16:47:02 +0800

    another test

--- cloud/main.js
+++ cloud/main.js
@@ -1315,10 +1315,10 @@ function getSecurityFromAppObject(appObject) {
 Parse.Cloud.define("getDeveloperFromUserId", async (request) => {
   const { siteId, userId } = request.params;
   try {
-    // const developer = await getDeveloperFromUserId(siteId, userId);
+    const developer = await getDeveloperFromUserId(siteId, userId);
     const isMuralAdmin = await checkIfMuralAdmin(siteId, userId);
-    // return { status: 'success', developer, isMuralAdmin };
-    return { status: 'success', isMuralAdmin };
+    return { status: 'success', developer, isMuralAdmin };
+    // return { status: 'success', isMuralAdmin };
   } catch (error) {
     console.log('inside getDeveloperFromUserId', error);
     return { status: 'error', error };
@@ -1374,7 +1374,7 @@ const checkIfMuralAdmin = async(userId) => {
     roleQuery.include('users')
     const roleObject = await roleQuery.find();
     if (!roleObject) return false;
-    console.log("------------------", roleObject.get('users'));
+    console.log("------------------", roleObject);
     return true;
   } catch(error) {
     console.error('inside checkIfMuralAdmin', error);
